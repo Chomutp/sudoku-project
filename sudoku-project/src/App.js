9a777a282c54c89e41f63a374f533a0cc953e967
uthumporn naikorn <utp.naikorn@gmail.com>
2019-12-24 10:07:59 +0700

    sudoku-isInitial

--- sudoku-project/src/App.js
+++ sudoku-project/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Cell extends Component {
+  state = {
+    number: 1
+  };
+  render() {
+    return (
+      <div
+        onClick={e => {
+          this.setState({
+            number: (this.state.number + 1) % 5
+          });
+        }}
+        className={`cell ${this.props.isInitial ? "initial" : ""}`}
+      >
+        {this.state.number !== 0 && this.state.number}
+      </div>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="board">
+          <Cell number={2} isInitial />
+          <Cell number={3} />
+          <Cell number={4} />
+          <Cell number={1} />
+          <Cell number={2} />
+          <Cell number={3} />
+          <Cell number={4} />
+          <Cell number={1} />
+          <Cell number={2} />
+          <Cell number={3} />
+          <Cell number={4} />
+          <Cell number={1} />
+          <Cell number={2} />
+          <Cell number={3} />
+          <Cell number={4} />
+          <Cell number={1} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
