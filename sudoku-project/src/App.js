c669dc03d8942c22228eb49db0e9b7c9923ede65
uthumporn naikorn <utp.naikorn@gmail.com>
2019-12-30 23:44:42 +0700

    sudoku game

--- sudoku-project/src/App.js
+++ sudoku-project/src/App.js
@@ -1,21 +1,92 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const validate = board => {
+  let isValid = true;
+  for (let i = 0; i < 4; i++) {
+    const horizontal = new Set();
+    const vertical = new Set();
+    const square = new Set();
+    for (let j = 0; j < 4; j++) {
+      horizontal.add(board[i][j]);
+      vertical.add(board[j][i]);
+      square.add(
+        board[2 * (i % 2) + (j % 2)][2 * Math.floor(i / 2) + Math.floor(j / 2)]
+      );
+    }
+    horizontal.delete(0);
+    vertical.delete(0);
+    square.delete(0);
+    if (horizontal.size !== 4 || vertical.size !== 4 || square.size !== 4) {
+      isValid = false;
+    }
+  }
+  return isValid;
+};
 class Cell extends Component {
-  state = {
-    number: 1
-  };
   render() {
     return (
       <div
         onClick={e => {
-          this.setState({
-            number: (this.state.number + 1) % 5
-          });
+          if (this.props.isInitial) {
+            return;
+          }
+          this.props.onChange((this.props.number + 1) % 5);
         }}
         className={`cell ${this.props.isInitial ? "initial" : ""}`}
       >
-        {this.state.number !== 0 && this.state.number}
+        {this.props.number !== 0 && this.props.number}
+      </div>
+    );
+  }
+}
+
+class Board extends Component {
+  state = {
+    board: [
+      [1, 2, 3, 4],
+      [3, 4, 0, 0],
+      [2, 0, 4, 0],
+      [4, 0, 0, 2]
+    ],
+    initial: [
+      [true, true, true, true],
+      [true, true, false, false],
+      [true, false, true, false],
+      [true, false, false, true]
+    ],
+    statusText: ""
+  };
+  summit = () => {
+    const isValid = validate(this.state.board);
+    this.setState({
+      statusText: isValid ? "Board is complete :)" : "Board is invalid :("
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="board">
+          {this.state.board.map((row, i) =>
+            row.map((number, j) => (
+              <Cell
+                key={`cell-${i}-${j}`}
+                isInitial={this.state.initial[i][j]}
+                number={number}
+                onChange={newNumber => {
+                  const { board } = this.state;
+                  board[i][j] = newNumber;
+                  this.setState({
+                    board
+                  });
+                }}
+              />
+            ))
+          )}
+        </div>
+        <button onClick={this.summit}>Summit</button>
+        <p>{this.state.statusText}</p>
       </div>
     );
   }
@@ -24,24 +95,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="board">
-          <Cell number={2} isInitial />
-          <Cell number={3} />
-          <Cell number={4} />
-          <Cell number={1} />
-          <Cell number={2} />
-          <Cell number={3} />
-          <Cell number={4} />
-          <Cell number={1} />
-          <Cell number={2} />
-          <Cell number={3} />
-          <Cell number={4} />
-          <Cell number={1} />
-          <Cell number={2} />
-          <Cell number={3} />
-          <Cell number={4} />
-          <Cell number={1} />
-        </div>
+        <Board />
       </div>
     );
   }
